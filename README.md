b3d347f07c64a636dd09ed82679bc3d5217c1911
Octavian Nita <mnothro@yahoo.com>
2014-09-01 16:55:04 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+cryptophoto-node-demo
+=====================
+
+Securing an Express application using the [CryptoPhoto](http://cryptophoto.com/) [API](https://cryptophoto.com/admin/api).
---
+++ app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var
+  express = require('express'),
+  app = expres(), server;
+
+app.get('/', function(req, res) {
+  res.send('Hello world!');
+});
+
+server = app.listen(3000, function() {
+  console.log('Server started; listening on port %d', server.address().port);
+});
