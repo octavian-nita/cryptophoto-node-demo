e8e543a15d0931b82689808aa5262e531a332131
Octavian Nita <mnothro@yahoo.com>
2014-09-02 17:22:28 +0200

    functional express web app

--- app.js
+++ app.js
@@ -2,11 +2,29 @@
 
 var
   express = require('express'),
-  app = express(), server;
+  path = require('path'),
+  app, server;
 
-app.get('/', function(req, res) {
-  res.send('Hello world!');
+app = express();
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(express.static(path.join(__dirname, 'public'), { maxage: '6h' }));
+
+app.use('/', function(req, res) {
+  res.render('internal', { userId: 'Octavian' });
 });
+
+// app.use(function(req, res, next) { // catch 404 and forward to error handler
+//   var err = new Error('Not Found');
+//   err.status = 404;
+//   next(err);
+// });
+//
+// app.use(function(err, req, res, next) {
+//   res.status(err.status || 500);
+//   res.render('login', { errorMessage: err.message });
+// });
 
 server = app.listen(3000, function() {
   console.log('Server started; listening on port %d', server.address().port);
