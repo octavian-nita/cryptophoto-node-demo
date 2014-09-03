f34744fa0bf95945a58a723c3c64d2b5f749ce47
Octavian Nita <mnothro@yahoo.com>
2014-09-03 16:52:11 +0200

    + login form

--- app.js
+++ app.js
@@ -12,19 +12,20 @@ app.set('view engine', 'jade');
 app.use(express.static(path.join(__dirname, 'public'), { maxage: '6h' }));
 
 app.use('/', function(req, res) {
-  res.render('internal', { userId: 'Octavian' });
+  //res.render('internal', { userId: 'Octavian' });
+  res.render('login');
 });
 
-// app.use(function(req, res, next) { // catch 404 and forward to error handler
-//   var err = new Error('Not Found');
-//   err.status = 404;
-//   next(err);
-// });
-//
-// app.use(function(err, req, res, next) {
-//   res.status(err.status || 500);
-//   res.render('login', { errorMessage: err.message });
-// });
+app.use(function(req, res, next) { // catch 404 and forward to error handler
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('login', { errorMessage: err.message });
+});
 
 server = app.listen(3000, function() {
   console.log('Server started; listening on port %d', server.address().port);
