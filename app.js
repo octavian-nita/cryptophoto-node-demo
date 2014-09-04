def454f5809e69ad21678e4c065401a025c358d9
Octavian Nita <mnothro@yahoo.com>
2014-09-04 16:44:31 +0200

    something is wrong with POST params

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 var
   path = require('path'),
   express = require('express'),
+  bodyParser = require('body-parser'),
   session = require('express-session'),
   cryptophoto = require('cryptophoto-node'),
 
@@ -22,6 +23,8 @@ cryptophoto.visibleIp(function(error, visibleIp) {
 
   if (error) { return console.error(error.toString()); }
 
+  // SERVER SETUP:
+
   cpClient = cryptophoto.createClient(process.env.CP_PUBLIC_KEY || 'efe925bda3bc2b5cd6fe3ad3661075a7',
                                       process.env.CP_PRIVATE_KEY || '384b1bda2dafcd909f607083da22fef0');
 
@@ -29,38 +32,97 @@ cryptophoto.visibleIp(function(error, visibleIp) {
   app.set('view engine', 'jade');
   app.set('views', path.join(__dirname, 'views'));
 
-  app.use(session({ maxAge: 60000, saveUninitialized: true, secret: 'my precious', resave: true}));
+  app.use(bodyParser.urlencoded({ extended: true }));
   app.use(express.static(path.join(__dirname, 'public'), { maxage: '6h' }));
+  app.use(session({ maxAge: 60000, saveUninitialized: true, secret: 'my precious', resave: true}));
 
   // Routes and handlers:
 
   app.get('/', function(request, response) {
     var session = request.session;
 
-    if (request.query.logout != null) {
+    if (request.query.logout != null) {   // allow log out using queries like '?logout'
       console.log('Logging out...');
       return session.destroy(function() {
         response.render('login', { errorMessage: '' });
       });
     }
 
-    return session.userId ? response.render('internal') : response.render('login', { errorMessage: '' });
+    session.userId ? response.render('internal') : response.render('login', { errorMessage: '' });
+  });
+
+  app.post('/', function(request, response, next) {
+    var session = request.session, userId, passWd;
+
+    if (!session.authPending) {
+      if (session.userId) { return response.render('internal'); }
+
+      // Check user id and password:
+      userId = request.body.userId ? request.body.userId.trim() : '';
+      passWd = request.body.passWd ? request.body.passWd.trim() : '';
+      if (DB[userId] !== passWd) {
+        return next(err('The username or password you entered is not correct! ( Try Moria/Mellon ;) )', 401));
+      }
+
+      // Establish a valid CryptoPhoto session:
+      cpClient.getSession(userId, visibleIp, function(error, cpSession) {
+        if (error) { return next(error); }
+
+        if ('true' !== String(cpSession.valid)) {
+          return next(err('CryptoPhoto session is not valid (' + cpSession.error + ')'));
+        }
+
+        session.userId = userId;
+        session.authPending = true;
+
+        // Display CryptoPhoto widget (either challenge or token generation):
+        if (cpSession.token) {
+          response.render('login', { userId: userId, cryptoPhotoWidget: cpClient.getChallengeWidget(cpSession) });
+        } else {
+          session.destroy(function() {
+            response.render('login',
+                            { userId: userId, cryptoPhotoWidget: cpClient.getTokenGenerationWidget(cpSession) });
+          }); // simplify a bit the flow... after token generation, the user must re-log in
+        }
+      });
+
+    } else { // the user has just responded to the challenge:
+      cpClient.verify(request.body.selector, request.body.row, request.body.col, request.body.cph, session.userId,
+                      visibleIp, function(error, cpVerification) {
+          if (error) { return session.destroy(function() { next(error); }); }
+
+          if ('true' !== String(cpVerification.valid)) {
+            return session.destroy(function() {
+              next(err('CryptoPhoto verification failed (' + cpVerification.error + ')'));
+            });
+          }
+
+          session.authPending = false;
+          response.render('internal');
+        });
+    }
   });
 
   app.use(function(req, res, next) { // catch 404 and forward to error handler
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+    next(err('Sorry, the page you were looking for was not found.', 404));
   });
 
-  app.use(function(err, req, res, next) {
+  app.use(function(err, req, res, next) { // declaring 4 arguments indicates an express error handler!
     res.status(err.status || 500);
     res.render('login', { errorMessage: err.message });
   });
 
-  // Server startup:
+  // SERVER STARTUP:
 
   server = app.listen(3000, function() {
     console.log('Server started; %s:%d', visibleIp, server.address().port);
   });
+
+  // UTILITIES:
+
+  function err(errorMessage, status) {
+    var err = new Error(errorMessage);
+    err.status = status || 500;
+    return err;
+  }
 });
